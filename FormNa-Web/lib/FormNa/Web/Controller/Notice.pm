cb9be76784cf935d2a67aa67b6227a16f719a5b4
Minsun Lee <mintegrals@gmail.com>
2011-12-02 16:12:17 +0900

    Create a skel

--- FormNa-Web/lib/FormNa/Web/Controller/Notice.pm
+++ FormNa-Web/lib/FormNa/Web/Controller/Notice.pm
@@ -16,17 +16,56 @@ Catalyst Controller.
 
 =cut
 
-
 =head2 index
 
 =cut
 
-sub index :Path :Args(0) {
+sub index :Chained('/') :PathPart('notice') :CaptureArgs(0){
     my ( $self, $c ) = @_;
-
-    $c->response->body('Matched FormNa::Web::Controller::Notice in Notice.');
 }
 
+=head2 form_index
+
+Display form to web
+
+=cut
+
+sub form_index :Chained('index') :PathPart('') :Args(0) {
+    my ($self, $c) = @_;
+}
+
+=head2 form_create_do
+
+Take information from form and add to odt file
+
+=cut
+
+sub form_create_do :Chained('index') :PathPart('form_create_do') :Args(0) {
+    my ($self, $c) = @_;
+    
+    my $name            = $c->req->param('name');
+    
+    my %formna_config;
+    
+    $formna_config{templates}{'content.xml'} = {
+        name          => $name,
+    };
+
+    my $tpl_dir = sprintf "%s/templates", $c->config->{odt}{root_};
+    my $src = sprintf "%s/template.odt", $c->config->{odt}{root_};
+    my $time = time;
+    my $dst = sprintf "%s/result/%s.odt", $c->config->{odt}{root_}, $time;
+    
+    my $odt = OpenDocument::Template->new(
+        config       => \%formna_config,
+        template_dir => $tpl_dir,
+    	  src          => $src,
+    	  dest         => $dst,
+    );
+    $odt->generate;
+    $c->log->debug("Generated $dst");
+
+}
 
 =head1 AUTHOR
 
