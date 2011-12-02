e657d9d2fd0164282725aa3e9d69bc51bd874d18
Minsun Lee <mintegrals@gmail.com>
2011-12-02 19:55:19 +0900

    Parameter receive code

--- FormNa-Web/lib/FormNa/Web/Controller/Award.pm
+++ FormNa-Web/lib/FormNa/Web/Controller/Award.pm
@@ -43,12 +43,32 @@ Take information from form and add to odt file
 sub form_create_do :Chained('index') :PathPart('form_create_do') :Args(0) {
     my ($self, $c) = @_;
     
-    my $name            = $c->req->param('name');
+    my $name            = $c->req->param('name')
+    my $num             = $c->req->param('num');
+    my $subject         = $c->req->param('subject');
+    my $award_name      = $c->req->param('award_name');
+    my $receiver        = $c->req->param('receiver');
+    my $comment         = $c->req->param('comment');
+    my $year            = $c->req->param('year');
+    my $month           = $c->req->param('month');
+    my $day             = $c->req->param('day');
+    my $organ           = $c->req->param('organ');
+    my $organ_president = $c->req->param('organ_president');
     
     my %formna_config;
     
     $formna_config{templates}{'content.xml'} = {
-        name          => $name,
+        name            => $name,
+        num             => $num            
+        subject         => $subject        
+        award_name      => $award_name     
+        receiver        => $receiver       
+        comment         => $comment        
+        year            => $year           
+        month           => $month          
+        day             => $day            
+        organ           => $organ          
+        organ_president => $organ_president
     };
 
     my $tpl_dir = sprintf "%s/templates", $c->config->{odt}{root_award};
