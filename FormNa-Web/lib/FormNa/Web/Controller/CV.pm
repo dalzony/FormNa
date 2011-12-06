619c201f945b0eeec23145529c1273ce1ef5ab53
Minsun Lee <mintegrals@gmail.com>
2011-12-06 19:46:46 +0900

    Copy and paste

--- FormNa-Web/lib/FormNa/Web/Controller/CV.pm
+++ FormNa-Web/lib/FormNa/Web/Controller/CV.pm
@@ -16,17 +16,65 @@ Catalyst Controller.
 
 =cut
 
-
 =head2 index
 
 =cut
 
-sub index :Path :Args(0) {
+sub index :Chained('/') :PathPart('cv') :CaptureArgs(0){
     my ( $self, $c ) = @_;
-
-    $c->response->body('Matched FormNa::Web::Controller::CV in CV.');
 }
 
+=head2 form_index
+
+Display form to web
+
+=cut
+
+sub form_index :Chained('index') :PathPart('') :Args(0) {
+    my ($self, $c) = @_;
+}
+
+=head2 form_create_do
+
+Take information from form and add to odt file
+
+=cut
+
+sub form_create_do :Chained('index') :PathPart('form_create_do') :Args(0) {
+    my ($self, $c) = @_;
+
+    my $subject         = $c->req->param('subject');
+    my $name            = $c->req->param('name');
+    my %formna_config;
+
+    $formna_config{templates}{'content.xml'} = {
+        subject        => $subject,
+        short_comment  => $short_comment,
+        detail_comment => $detail_comment,
+        picture        => $picture,
+        phone          => $phone,
+        name           => $name,
+    };
+
+    my $tpl_dir = sprintf "%s/templates", $c->config->{odt}{root_cv};
+    my $src = sprintf "%s/template.odt", $c->config->{odt}{root_cv};
+    my $time = time;
+    my $dst = sprintf "%s/result/%s.odt", $c->config->{odt}{root_cv}, $time;
+
+    my $odt = OpenDocument::Template->new(
+        config       => \%formna_config,
+        template_dir => $tpl_dir,
+        src          => $src,
+        dest         => $dst,
+    );
+    $odt->generate;
+    $c->log->debug("Generated $dst");
+    $c->res->headers->content_type('application/msword');
+    $c->res->headers->header("Content-Disposition" => 'attachment;filename="' . "$time.doc" . '";');
+    my $fh = IO::File->new( $dst, 'r' );
+    $c->res->body($fh);
+    undef $fh;
+}
 
 =head1 AUTHOR
 
