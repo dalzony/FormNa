305b67222c38d2e1c105dd8fef4d74fe80702d4d
Hojung Youn <amoc.yn@gmail.com>
2011-12-08 01:48:50 +0900

    [#15] Add wrapper and css files

--- FormNa-Web/lib/FormNa/Web.pm
+++ FormNa-Web/lib/FormNa/Web.pm
@@ -40,6 +40,7 @@ __PACKAGE__->config(
     name => 'FormNa::Web',
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
+    wrapper => 'wrapper.tt',
 );
 
 # Start the application
